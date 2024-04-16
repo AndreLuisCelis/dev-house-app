da55c0759c5aa9e628bd018f60292589d9ce70b8
Andre Luis Celis <andre.celis@meta.com.br>
2024-04-16 10:59:43 -0300

    fix: added nginx.conf for refresh page error 404

--- api-Dev-House/src/controllers/HouseController.js
+++ api-Dev-House/src/controllers/HouseController.js
@@ -57,13 +57,13 @@ class HouseController{
   }
 
   async destroy(req, res){
-    const { house_id } = req.body;
-    const { user_id } = req.headers;
+    const { house_id } = req?.body;
+    const { user_id } = req?.headers;
 
     const user = await User.findById(user_id);
     const houses = await House.findById(house_id);
 
-    if(String(user._id) !== String(houses.user)){
+    if(String(user?._id) !== String(houses?.user)){
       return res.status(401).json({ error: 'Não autorizado.'});
     }
 
